91477a20f5e19b5c647bc190a6fe016cd3a943a0
TerrenceMM2 <terrencemm2@gmail.com>
2019-07-21 22:38:09 -0500

    adds html elements/styling, api get for html files and assets folder

---
+++ app/data/friends.js
@@ -0,0 +1,58 @@
+var friendsArray = [
+    {
+        "name": "Pete",
+        "photo": "",
+        "scores": [
+            3,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Josh",
+        "photo": "",
+        "scores": [
+            5,
+            2,
+            4
+        ]
+    },
+    {
+        "name": "Patricia",
+        "photo": "",
+        "scores": [
+            5,
+            5,
+            2
+        ]
+    },
+    {
+        "name": "Priscila",
+        "photo": "",
+        "scores": [
+            1,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Micaela",
+        "photo": "",
+        "scores": [
+            4,
+            2,
+            5
+        ]
+    },
+    {
+        "name": "Gavin",
+        "photo": "",
+        "scores": [
+            3,
+            3,
+            3
+        ]
+    },
+];
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -7,12 +7,11 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/assets", express.static("assets"));
 
-
-
-
-
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+});

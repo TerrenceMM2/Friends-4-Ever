a07a5ab58f803e1f5133d6efa4d17e56eb272cbf
TerrenceMM2 <terrencemm2@gmail.com>
2019-07-22 21:27:47 -0500

    adds more questions to total 10

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,56 +2,33 @@ var friendsArray = [
     {
         "name": "Pete",
         "photo": "",
-        "scores": [
-            3,
-            4,
-            1
-        ]
+        "scores": 
+        [3, 4, 1, 5, 3, 3, 2, 1, 4, 4]
     },
     {
         "name": "Josh",
         "photo": "",
-        "scores": [
-            5,
-            2,
-            4
-        ]
+        "scores": [1, 2, 1, 2, 1, 1, 4, 5, 2, 3]
     },
     {
         "name": "Patricia",
         "photo": "",
-        "scores": [
-            5,
-            5,
-            2
-        ]
+        "scores": [2, 2, 5, 5, 4, 3, 1, 2, 3, 4]
     },
     {
         "name": "Priscila",
         "photo": "",
-        "scores": [
-            1,
-            2,
-            3
-        ]
+        "scores": [5, 4, 4, 5, 5, 5, 4, 3, 2, 5]
     },
     {
         "name": "Micaela",
         "photo": "",
-        "scores": [
-            4,
-            2,
-            5
-        ]
+        "scores": [2, 5, 2, 1, 4, 3, 5, 2, 2, 1]
     },
     {
         "name": "Gavin",
         "photo": "",
-        "scores": [
-            3,
-            3,
-            3
-        ]
+        "scores": [3, 2, 4, 3, 2, 1, 5, 5, 3, 2]
     },
 ];
 
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,6 +8,13 @@ $(document).ready(function() {
         var response1 = $("#question1").val();
         var response2 = $("#question2").val();
         var response3 = $("#question3").val();
+        var response4 = $("#question4").val();
+        var response5 = $("#question5").val();
+        var response6 = $("#question6").val();
+        var response7 = $("#question7").val();
+        var response8 = $("#question8").val();
+        var response9 = $("#question9").val();
+        var response10 = $("#question10").val();
 
         var newFriend = {
             name: friendName,
@@ -16,6 +23,13 @@ $(document).ready(function() {
                 response1,
                 response2,
                 response3,
+                response4,
+                response5,
+                response6,
+                response7,
+                response8,
+                response9,
+                response10,
             ]
         };
 
@@ -24,9 +38,7 @@ $(document).ready(function() {
         $.post("/api/friends", newFriend, function() {
             $("#name-input").val("");
             $("#photo-file").val("");
-            $("#question1").val(3);
-            $("#question2").val(3);
-            $("#question3").val(3);
+            $(".slider").val(3);
         }).then(function(res) {
             console.log(res);
             $("#friend-modal-body").text(res);
